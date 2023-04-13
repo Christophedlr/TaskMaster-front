d03f329e01835d2a750fb83b7b4dfb670b93d2c5
Christophedlr <christophedlr@gmail.com>
2023-04-13 21:56:30 +0200

    Bootstrap Component Wrapper
    - Adding Navbar

---
+++ src/components/Bootstrap/Components/Navbar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Container from "../Layout/Container";
+
+export class Navbar extends React.Component {
+    render() {
+        return (
+            <>
+                <nav className={`navbar navbar-expand-${(this.props.expand) ? this.props.expand : "lg"} ${(this.props.className) ? this.props.className : ""}`} data-bs-toggle={"collapse"} data-bs-target={`#${(this.props.target) ? this.props.target : "navbar"}`}>
+                    <Container type={"fluid"}>
+                        <a href={this.props.link} className="navbar-brand">{this.props.title}</a>
+                        <button className="navbar-toggler"><span className="navbar-toggler-icon"/></button>
+                        <div className="collapse navbar-collapse" id={`${(this.props.target) ? this.props.target : "navbar"}`}>
+                            <ul className={"navbar-nav"}>
+                                {this.props.children}
+                            </ul>
+                        </div>
+                    </Container>
+                </nav>
+            </>
+        );
+    }
+}
+
+export class NavbarElement extends React.Component {
+    render() {
+    if (this.props.type==="dropdown") {
+        return (
+            <>
+                <li className={`nav-item dropdown ${(this.props.className) ? this.props.className : ""}`}>
+                    <a href={"#"} role={"button"} data-bs-toggle={"dropdown"} className={"nav-link dropdown-toggle"}>
+                        {this.props.text}
+                    </a>
+
+                    <ul className={"dropdown-menu"}>
+                        {this.props.children}
+                    </ul>
+                </li>
+            </>
+        )
+    } else if (this.props.type==="dropdown-item") {
+            return (
+                <>
+                    <li>
+                        <a href={this.props.link} className={`dropdown-item ${(this.props.className ? " "+this.props.className : "")}`}>
+                            {this.props.text}
+                        </a>
+                    </li>
+                </>
+            )
+        } else if (this.props.type==="dropdown-hr") {
+            return (
+                <>
+                    <li><hr className={`dropdown-divider ${this.props.className}`} /></li>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <li className="nav-item">
+                        <a href={this.props.link} className={`nav-link${(this.props.className ? " "+this.props.className : "")}`}>{this.props.text}</a>
+                    </li>
+                </>
+            );
+        }
+    }
+}
